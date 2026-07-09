400656889d9df0b74c5bf7f858a9c6a0e484f07a
SashaDesjardins <2368398@cegepmontpetit.ca>
2026-07-09 16:21:07 -0400

    FCT: Added roll ranks
    Added rank of roles from the FATE core system

--- HelloOwlbear/src/counter.js
+++ HelloOwlbear/src/counter.js
@@ -13,16 +13,37 @@ export async function setupCounter(element,name) {
     }
     const modifier = Number(document.getElementById("modifier").value);
     counter += modifier;
-    const time = new Date().toLocaleTimeString();
+      function getRollRank(counter) {
+  if (counter > 8) return "Beyond Legendary";
+
+  switch (counter) {
+    case 8: return "Legendary";
+    case 7: return "Epic";
+    case 6: return "Fantastic";
+    case 5: return "Superb";
+    case 4: return "Great";
+    case 3: return "Good";
+    case 2: return "Fair";
+    case 1: return "Average";
+    case 0: return "Mediocre";
+    case -1: return "Poor";
+    case -2: return "Terrible";
+
+    default:
+      return "Below Terrible";
+  }
+}
+   const rollRank = getRollRank(counter);
+    const time = new Date().toISOString().slice(0, 19).replace("T", " ");
     const li = document.createElement("li");
     li.textContent = `${time} | ${name} rolled ${counter} (${rolls.join(", ")})`;
     document.getElementById("rollHistory").prepend(li);
     element.innerHTML = `count is ${counter}`;
-     OBR.notification.show(`${name} just rolled a ${counter}, nice!`);
+     OBR.notification.show(`${name} just rolled a ${counter}, ${rollRank}!`);
      const rollData = {
   name: name,
   roll: counter,
-  time: new Date().toLocaleTimeString()
+  time: new Date().toISOString()
 };
 const data = await OBR.room.getMetadata();
 let history = data["com.HelloOwlbear.diceroller"]?.diceHistory || [];
